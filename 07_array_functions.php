84efc7f39f83cbbe033dd6abc19e6d774f54ad40
njokijacqueline <gatheca832@gmail.com>
2023-06-20 11:19:39 +0300

    updated

--- 07_array_functions.php
+++ 07_array_functions.php
@@ -5,3 +5,24 @@
   Functions to work with arrays
   https://www.php.net/manual/en/ref.array.php
 */
+
+//Get array length
+$fruits=array(
+  "Mango", "Banana", "Grapes", "Tangerine", "Oranges", "Apples", "Pears", "Watermelon", "Strawberries","Tree tomato"
+);
+//echo count($fruits);
+
+//Search element in array
+echo in_array("Pineapple", $fruits);
+
+
+//Adding an element to an array
+$fruits[]="kiwi";
+array_push($fruits, "thorn melon", "dates", "coconut", "groundnuts");
+print_r($fruits);
+
+//Removing an element from an array
+array_pop($fruits);//Function to remove last element in the array
+array_shift($fruits);//Function to remove the first element in an array
+unset($fruits[2]);//Removes specific element in an array
+print_r($fruits);
--- 08_string_functions.php
+++ 08_string_functions.php
@@ -5,3 +5,15 @@
   Functions to work with strings
   https://www.php.net/manual/en/ref.strings.php
 */
+
+$string="Hello world";
+
+//Getting the length of the string
+echo strlen($string);
+
+
+//reversing a string
+echo strrev($string);
+
+//converting all characters to upper case
+echo strtoupper($string);
